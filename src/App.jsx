5e84e6fcac320f8384f6cd9753206e5c85799a28
Hassan Ahmed <hassanshakeel381@gmail.com>
2024-09-24 22:52:05 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import React, { useRef } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { addTodo, removeTodo , editTodo  } from './config/redux/reducers/todoSlice';
+
+
+const App = () => {
+
+  const todoVal = useRef();
+
+  const dispatch = useDispatch()
+
+  const selector = useSelector(state  => state.todos.todo)
+  console.log(selector);
+  
+  const addToDoInRedux = (e) => {
+    e.preventDefault();
+    console.log("Todo Added =>", todoVal.current.value);
+    dispatch(addTodo({
+      title: todoVal.current.value
+    }))
+    todoVal.current.value = ""
+  }
+
+  const deleteItemFromRedux = (index) => {
+    console.log("delete" , index);
+    dispatch(removeTodo({
+      index 
+    }))
+    
+  }
+
+  const editItemFromRedux = (index) => {
+    const editValue = prompt('new value')
+    dispatch(editTodo({
+      index,
+      title: editValue,
+    })) 
+  }
+
+  // const editItemFromRedux = (index) => {
+  //   const editValue = prompt('Enter the updated value');
+  //   if (editValue) {
+  //     dispatch(
+  //       editTodo({
+  //         index,
+  //         title: editValue,
+  //       })
+  //     );
+  //   }
+  // };
+
+
+  return (
+    <>
+      <form>
+        <input type="text" ref={todoVal}/>
+        <button onClick={addToDoInRedux} type='submit'>Add todo</button>
+      </form> 
+           {selector.length > 0 ? selector.map((item , index) => {
+            return <div key={item.id}>
+              <p>{item.title}
+              <button onClick={()=> deleteItemFromRedux(index)}>delete</button>
+              <button onClick={()=> editItemFromRedux(index)}>edit</button></p>
+            </div>
+          }) : <h1>No data found</h1>}
+    </>
+  )
+}
+
+export default App
---
+++ src/config/redux/reducers/todoSlice.js
@@ -0,0 +1,27 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+export const todoSlice = createSlice({
+  name: "Todos",
+  initialState: {
+    todo: [],
+  },
+  reducers: {
+    addTodo: (state, action) => {
+      state.todo.push({
+        title: action.payload.title,
+        id: nanoid(),
+      });
+    },
+    removeTodo: (state, action) => {
+      state.todo.splice(action.payload.index, 1);
+    },
+    editTodo: (state, action) => {
+        // Find the todo at the given index and update its title
+        const todoToEdit = state.todo[action.payload.index];
+            todoToEdit.title = action.payload.title; // Update the title
+    }
+  },
+});
+
+export const { addTodo, removeTodo, editTodo } = todoSlice.actions;
+export default todoSlice.reducer;
